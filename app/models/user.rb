aa069dc72be668507e45ffbc59e9a74b8d9af5b9
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-27 14:38:35 +0900

    EMailのdowncaseテストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
             presence: true,
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false };
+            uniqueness: { case_sensitive: false }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,12 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    # DBに保存される段階でdowncaseされるのでreloadメソッドを呼ぶことでDBの値を得る
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
