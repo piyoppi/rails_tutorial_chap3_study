816458d52d4be4e7d259d832ebc951d323e53da3
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-27 16:00:23 +0900

    ユーザー認証用としてpasswordが利用できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,10 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
+
   has_secure_password
 
 end
