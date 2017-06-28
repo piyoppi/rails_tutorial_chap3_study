d3c9348ef321157cc7d8cf19907737d30cb7f415
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-28 12:27:18 +0900

    サインアップ成功時のテストを記述

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,8 +6,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post signup_path, params: {
-        user: 
-        {
+        user: {
           name: "",
           email: "user@invalid",
           password: "foo",
@@ -25,6 +24,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'li', "Password is too short (minimum is #{User::MIN_PASSWORD_LENGTH} characters)"
 
     assert_select 'form[action=?]', "/signup"
-
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+           name: "Example User",
+           email: "user@example.com",
+           password: "password",
+           password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
