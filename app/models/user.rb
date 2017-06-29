abbbb7f473d7081db855df98efb1c2adb3e73b02
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-29 15:07:46 +0900

    ユーザーの永続的ログイン機能の追加とログアウト機能の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,21 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # 渡されたトークンがダイジェストと一致しているかを確認
+  # 戻り値： true -> 一致
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil);
+  end
+
   class << self
 
     def digest(string)
@@ -30,11 +45,6 @@ class User < ApplicationRecord
 
     def new_token
       SecureRandom.urlsafe_base64
-    end
-
-    def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
     end
 
   end
