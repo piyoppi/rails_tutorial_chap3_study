b42b405ef30253fcd3c248a16c9853682b4b5d2b
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-30 14:56:25 +0900

    ユーザー削除機能の追加と管理者権限周りのテストを追加

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,6 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index including pagination" do
@@ -14,5 +15,15 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
   end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
