d48edf950cf2e055dfbf0a2d875536475187615d
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-03 16:20:48 +0900

    Activationまわりのテストを追加した

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -26,7 +30,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action=?]', "/signup"
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {
@@ -38,9 +42,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }
       }
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    #有効化していない状態でのログイン
+    log_in_as(user)
+    assert_not is_logged_in?
+    #有効化トークンが不正
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    #トークンは正しいがメールアドレスが無効
+    get edit_account_activation_path(user.activation_token, email: 'wrong') 
+    assert_not is_logged_in?
+    #トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email) 
+    assert user.reload.activated?
+
     follow_redirect!
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.empty?
   end
 
