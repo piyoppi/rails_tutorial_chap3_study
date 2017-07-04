b02fa0fe693c4ff9d52d90d574b11a7b513db6cf
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-04 16:03:58 +0900

    Micropostのバリデーションを記述

--- db/migrate/20170704063614_create_microposts.rb
+++ db/migrate/20170704063614_create_microposts.rb
@@ -3,8 +3,8 @@ class CreateMicroposts < ActiveRecord::Migration[5.0]
     create_table :microposts do |t|
       t.text :content
       t.references :user, foreign_key: true
-
       t.timestamps
     end
+    add_index :microposts, [:user_id, :created_at]
   end
 end
