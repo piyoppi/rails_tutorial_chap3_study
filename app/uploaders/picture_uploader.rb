21c6bd16dba7775021eb64f57e6e67b0ba0cce9c
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-05 16:32:21 +0900

    本番環境用の記述を追加

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,8 +4,18 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
   storage :file
 
+  if Rails.env.production?
+    storage :aws
+  else
+    storage :file
+  end
+
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env.production?
+      "training/gatchan/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   def extension_whitelist
