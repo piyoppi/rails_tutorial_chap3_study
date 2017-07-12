3237d2cdfbb716c480551186b4252c59ff164fab
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-12 15:06:53 +0900

    JWT生成処理及び受信したトークン処理をruby-jwtを用いる実装に変更

--- app/helpers/application_interface_auth_helper.rb
+++ app/helpers/application_interface_auth_helper.rb
@@ -1,28 +1,34 @@
-require 'base64'
+require 'jwt'
 
 module ApplicationInterfaceAuthHelper
 
   def log_in_api(user)
     secret_key = "asdfjkhvlerADFGweohfgawoe"
-    header = {type: "JWT", alg: "HS256"}
-    payload = {iss: "example.com", exp: 1466839180, user_id: 33}
-    header_base64 = Base64.urlsafe_encode64(header.to_s)
-    payload_base64 = Base64.urlsafe_encode64(payload.to_s)
-    signature = Digest::SHA256.hexdigest("#{header_base64}.#{payload_base64}.#{secret_key}")
-    api_token = "#{header_base64}.#{payload_base64}.#{signature}"
-    user.update_attribute(:api_digest, api_token);
-    return api_token
+    expire_sec = 3600
+    payload = {iss: "example.com", exp: Time.now.to_i + expire_sec, user_id: user.id}
+    return JWT.encode payload, secret_key, 'HS256'
   end
 
   def log_out_api(token)
-    user = User.find_by(api_digest: token)
+    user = required_user(token)
     raise UserActivateError, "token is not found." if !user
     user.update_attribute(:api_digest, nil);
   end
 
   def logged_in_api?(token)
-    !!User.where(api_digest: token); 
+    !!required_user(token)
   end
+
+  private
+    
+    def decoded_token(token)
+      JWT.decode token, secret_key, true, algorithm: 'HS256'
+    end
+
+    def required_user(token)
+      token_params = decoded_token(token)
+      @required_user = User.find_by(id: token_params[:id])
+    end
 
 end
 
