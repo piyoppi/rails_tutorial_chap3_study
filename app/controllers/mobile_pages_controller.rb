2b52bd56906e63bbf7c48ca89a5f964890b4720b
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-14 11:50:20 +0900

    vuejsをインストール

---
+++ app/controllers/mobile_pages_controller.rb
@@ -0,0 +1,4 @@
+class MobilePagesController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'mobile_pages/home'
+
   root 'static_pages#home'
   get 'password_resets/new'
   get 'password_resets/edit'
