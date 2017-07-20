e69b8e3dfe5fb14a8bb734e6dcd37ef772a7e41b
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-20 14:20:42 +0900

    URL表示にハッシュ記号がつかないように変更

--- app/javascript/packs/root.vue
+++ app/javascript/packs/root.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="root">
         <p>
-        <router-link to="/user">Go to feed</router-link>
-        <router-link to="/login">Go to login</router-link>
+        <router-link to="/m/user">Go to feed</router-link>
+        <router-link to="/m/login">Go to login</router-link>
         </p>
         <router-view></router-view>
     </div>
