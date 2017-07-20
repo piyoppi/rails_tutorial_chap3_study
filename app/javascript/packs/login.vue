76bf2d36fbf48aea08120c9f91a40cbaf17bd3fa
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-20 14:59:33 +0900

    login.vueのインデント修正

--- app/javascript/packs/login.vue
+++ app/javascript/packs/login.vue
@@ -1,26 +1,26 @@
 <template>
-  <div id="login">
-      <div id="information">
-          {{ message }}
-      </div>
-      <div id="login_outer">
-          Username: <input type="text" v-model="email" />
-          Password: <input type="password" v-model="password" />
-          <button v-on:click="request_auth()">next</button>
-      </div>
-  </div>
+    <div id="login">
+        <div id="information">
+            {{ message }}
+        </div>
+        <div id="login_outer">
+            Username: <input type="text" v-model="email" />
+            Password: <input type="password" v-model="password" />
+            <button v-on:click="request_auth()">next</button>
+        </div>
+    </div>
 </template>
 
 <script>
 import Api from '../lib/micropost_api.js'
 export default {
     data: function(){
-            return{
-                email: "",
-                password: "",
-                message: ""
-            }
+        return{
+            email: "",
+            password: "",
+            message: ""
         }
+    }
     ,
     created: function(){
     },
