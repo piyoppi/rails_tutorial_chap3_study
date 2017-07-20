b7bf5858806a5f5a246473c5eb1b1c49b3472d94
Shohei Ogata <shohei.ogata@pepabo.com>
2017-07-20 14:21:27 +0900

    不要なファイルの削除

---
+++ app/javascript/lib/micropost_api.js
@@ -0,0 +1,49 @@
+const axios = require('axios')
+export default class Api{
+    constructor(){
+
+    }
+
+    static get_user_micropost(user_id, page, callback){
+        axios.get('/api/users/' + user_id, {
+            params: {
+                page: page
+            }
+        }).then(function(response){
+            callback( response.data.micropost );
+        })
+        .catch(function(error){
+            console.log(error);   
+        });
+    }
+
+    static get_users(page, callback){
+        //あとで実装する
+    }
+
+    static login(email, password, callback){
+        axios.post('/api/login/', {
+            email: email,
+            password: password
+        }).then(function(response){
+            localStorage.setItem("api_token", response.data.access_token);
+            callback(response.data);
+        })
+        .catch(function(error){
+            callback({message: "Invalid email/password combination"});
+        });
+    }
+
+    static get_userfeed(page, callback){
+        let api_token = localStorage.getItem("api_token");
+        axios.defaults.headers.common['Authorization'] = api_token;
+        axios.post('/api/feed/', {
+            page: page
+        }).then(function(response){
+            callback(response.data);
+        })
+        .catch(function(error){
+            callback({message: "Invalid request"});
+        });
+    }
+}
---
+++ app/javascript/packs/entry.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './app.vue'
+import Login from './login.vue'
+import Users from './users.vue'
+import Root from './root.vue'
+import VueRouter from 'vue-router'
+
+Vue.config.debug = true;
+Vue.use(VueRouter);
+Vue.component("root", Root);
+
+
+const routes = [
+    {path: '/m/user/:id', component: App},
+    {path: '/m/', component: Users},
+    {path: '/m/login', component: Login},
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.appendChild(document.createElement('root'))
+    let app = new Vue({
+        router: router
+    }).$mount('root');
+    console.log( app );
+})
+
+
